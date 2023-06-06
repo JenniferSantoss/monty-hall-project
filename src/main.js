fe3760038be01b4ce9c4e679b95aa06ae6f688ab
Jennifer Santos <jennylima10@gmail.com>
2023-06-06 13:17:00 -0300

    Problema de Monty Hall

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from './App'
 
-createApp(App).mount('#app')
+new Vue ({
+    render(createElement){
+        return createElement(App)
+    }
+}).$mount("#app")
